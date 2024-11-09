d8a7022a02f67acd36ed6d96c11c3a6f046580a8
name <tademg04@gmail.com>
2024-11-09 23:02:00 +0200

    completed calculator

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,105 @@ export const App = () => {
 	const [operand1, setOperand1] = useState("")
 	const [operator, setOperator] = useState("")
 	const [operand2, setOperand2] = useState("")
+	const [result, setResult] = useState("")
 
 	const NUMS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-	const resultDisplay = () => {
-		if (operand2.length !== 0 && operator === "+") {
-			setOperand1(Number(operand1) + Number(operand2))
+	const operandHandler = (number) => {
+		setResult("")
+
+		if (operand1.length === 19 || operand2.length === 19) {
+			return
+		}
+
+		if (!operator) {
+			setOperand1(prev => {
+				if(prev === '0'){
+					return number
+				}
+
+				return prev + number
+			})
+		} else {
+			setOperand2(prev => {
+				if(prev === '0'){
+					return number
+				}
+
+				return prev + number
+			})
+		}
+	}
+
+	const operatorHandler = (clickedOperator) => {
+		if (operand2) {
+			const result = operator === "+"
+				? String(Number(operand1) + Number(operand2))
+				: String(Number(operand1) - Number(operand2))
+
+			setOperand1(result)
 			setOperator("")
 			setOperand2("")
 		}
 
-		if (operand2.length !== 0 && operator === "-") {
-			setOperand1(Number(operand1) - Number(operand2))
-			setOperator("")
-			setOperand2("")
+		if (result) {
+			setOperand1(result)
+			setResult("")
+			setOperator(clickedOperator)
 		}
+
+		if (operand1) {
+			setOperator(clickedOperator)
+		}
+	}
+
+	const resultHandler = () => {
+		if (!operand2) {
+			return
+		}
+
+		const result = operator === "+"
+		? String(Number(operand1) + Number(operand2))
+		: String(Number(operand1) - Number(operand2))
+
+		setResult(result)
+		setOperand1("")
+		setOperator("")
+		setOperand2("")
+	}
+
+	const getScreenValue = () => {
+		if (operator) {
+			return operand2 || operand1
+		}
+
+		if (result) {
+			return result
+		}
+
+		return operand1 || "0"
 	}
 
 	return (
 		<div className={styles.main}>
             <div className={styles["calc-container"]}>
-				<p className={styles.screen}>{operand1} {operator} {operand2}</p>
+				<p className={styles.screen}>{getScreenValue()}</p>
 				<div className={styles["button-container"]}>
 					<button className={styles.button + " " + styles['reset-button']} onClick={() => {
 						setOperand1("")
 						setOperator("")
 						setOperand2("")
+						setResult("")
 					}}>C</button>
-					<button className={styles.button} onClick={resultDisplay}>=</button>
-					<button className={styles.button} onClick={() => {
-						if (operand1.length !== 0) {
-							setOperator("+")
-						}
-					}}>+</button>
-					<button className={styles.button} onClick={() => {
-						if (operand1.length !== 0) {
-							setOperator("-")
-						}
-					}}>-</button>
-					{NUMS.map((num, index) => <button
+					<button className={styles.button} onClick={resultHandler}>=</button>
+					<button className={styles.button} onClick={() => operatorHandler("+")}>+</button>
+					<button className={styles.button} onClick={() => operatorHandler("-")}>-</button>
+					{NUMS.map((num, index) =>
+						<button
 						className={styles.button}
 						key={index}
-						onClick={() => {
-							if (operator.length === 0) {
-								if (operand1 === "0") {
-									setOperand1("")
-								}
-
-								setOperand1(prev => prev + num)
-							} else {
-								if (operand2 === "0") {
-									setOperand2("")
-								}
-								
-								setOperand2(prev => prev + num)
-							}
-						}}>{num}</button>)}
+						onClick={() => operandHandler(num)}
+						>{num}</button>)}
 				</div>
 			</div>
 		</div>
